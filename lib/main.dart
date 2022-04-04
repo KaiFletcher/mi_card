9e446a162f69ddd55341572ae0def04fff333e56
Kai Fletcher <kaifletcher@gmail.com>
2022-04-04 19:49:27 +0000

    done

--- lib/main.dart
+++ lib/main.dart
@@ -10,22 +10,77 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.deepPurple,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 CircleAvatar(
-                  radius: 50.0,
+                  radius: 65.0,
                   backgroundImage: AssetImage('images/hunson.jpg'),
                 ),
                 Text(
                   'Kai Fletcher',
                   style: TextStyle(
+                    fontFamily: 'Caveat',
                     fontSize: 40.0,
-                    color: Colors.deepPurple,
+                    color: Colors.amber,
                     fontWeight: FontWeight.bold,
                   ),
-                )
+                ),
+                Text(
+                  'Flutter Developer',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 15.0,
+                    color: Colors.amber,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                SizedBox(
+                  width: 200.0,
+                  height: 20.0,
+                  child: Divider(
+                    color: Colors.amberAccent,
+                  ),
+                ),
+
+                Card(
+                  color: Colors.white38,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 35.0,
+                      color: Colors.amberAccent,
+                    ),
+                    title: Text('+354 697 7058',
+                      style: TextStyle(
+                        color: Colors.amberAccent.shade400,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white38,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 35.0,
+                      color: Colors.amberAccent,
+                    ),
+                    title: Text('kaifletcher@gmail.com',
+                      style: TextStyle(
+                        color: Colors.amberAccent.shade400,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
               ]
           ),
         ),
